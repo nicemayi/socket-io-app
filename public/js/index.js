e15a886fbb652a28082c0244ce46075fdb9e4817
nicemayi <wangzhe.dut@gmail.com>
2017-03-01 09:30:30 -0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,12 @@ socket.emit('createMessage', {
 
 jQuery("#message-form").on('submit', function(e) {
     e.preventDefault();
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-
+      messageTextbox.val('');
     });
 });
 
@@ -47,13 +48,15 @@ locationButton.on('click', function() {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 })
